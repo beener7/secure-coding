5b3430ff5902a7309ef3e0d92cc91f6b9c2bb69e
beener7 <104628675+beener7@users.noreply.github.com>
2025-04-21 22:28:31 +0900

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,13 @@
 # forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class RegisterForm(FlaskForm):
+    username = StringField('사용자명', validators=[DataRequired(), Length(min=4, max=32)])
+    password = PasswordField('비밀번호', validators=[DataRequired(), Length(min=6, max=32)])
+    confirm_password = PasswordField('비밀번호 확인', validators=[DataRequired(), EqualTo('password', message='비밀번호가 일치하지 않습니다.')])
+    submit = SubmitField('회원가입')
 
 class LoginForm(FlaskForm):
     username = StringField('사용자명', validators=[DataRequired(), Length(min=4, max=32)])
